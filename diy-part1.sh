8ef71f124218cf4cf390b25add092d9bd4aac83d
52popojie <76438601+52popojie@users.noreply.github.com>
2024-03-20 17:10:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git shidahuilang https://github.com/shidahuilang/openwrt-package' >>feeds.conf.default
+#echo 'src-git shidahuilang https://github.com/shidahuilang/openwrt-package' >>feeds.conf.default
